402b977453e05e5679c83785f90d91ab042b8713
Aashish Shilkande <83578134+Aashish257@users.noreply.github.com>
2025-10-04 14:44:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+ document.addEventListener('DOMContentLoaded', () => {
+    const messageDiv = document.createElement('div');
+    messageDiv.textContent = 'DOM load success';
+    messageDiv.className = 'text-4xl font-extrabold text-white bg-green-500 p-8 rounded-xl shadow-2xl transition duration-300 transform hover:scale-105 tracking-wider text-center';
+    document.body.appendChild(messageDiv);
+});
